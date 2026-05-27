7c5f100408eeb7e1493947f534e45be721d1b99c
AshwiniYadav9808 <yadavashwini280@gmail.com>
2026-05-27 17:33:40 +0530

    initialing myProject

---
+++ app/layout.tsx
@@ -0,0 +1,58 @@
+import type { Metadata } from "next";
+import { Schibsted_Grotesk, Martian_Mono, Geist } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import LightRays from '@/components/LightRays';
+import Navbar from "@/components/Navbar";
+
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
+
+const SchibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted_grotesk",
+  subsets: ["latin"],
+});
+
+const MartianMono = Martian_Mono({
+  variable: "--font-martian_mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DevEvent",
+  description: "The Hub for every dav Event You mustn't Miss",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+    >
+      <body
+        className={`${SchibstedGrotesk.variable} ${MartianMono.variable} min-h-screen antialiased`}>
+
+        <div className="absolute inset-0 top-0 a-[-1] min-h-screen ">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.2}
+            noiseAmount={0}
+            distortion={0.01}
+            className="custom-rays"
+          />
+        </div>
+        <Navbar/>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
