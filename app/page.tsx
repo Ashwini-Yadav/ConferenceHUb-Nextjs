a57230086761b6b0905eeac41deb27fee65a1aa7
AshwiniYadav9808 <yadavashwini280@gmail.com>
2026-06-04 15:48:53 +0530

    error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,29 @@
 import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
 import { IEvent } from "@/database";
+import { cacheLife } from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const page = async() => {
-const response = await fetch(`${BASE_URL}/api/events`,{ next: {revalidate: 3600}});
-const { events } = await response.json();
+const page = async () => {
+  'use cache';
+  cacheLife('hours')
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
 
   return (
     <section>
       <h1 className="text-center">The Hub For Every Dev <br /> Event You can't Miss</h1>
       <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in one place</p>
-      <ExploreBtn/>
+      <ExploreBtn />
 
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
 
         <ul className="events">
-          {events && events.length > 0 && events.map((event: IEvent)=> (
+          {events && events.length > 0 && events.map((event: IEvent) => (
             <li key={event.title} className="list-none">
-              <EventCard {...event}/>
+              <EventCard {...event} />
             </li>
           ))}
         </ul>
